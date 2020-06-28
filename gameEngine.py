945f2bfa11b83935eb62b21a92d69b3cbfbc12be
nafisgizatov <el_bruho@mail.ru>
2020-06-28 20:36:41 +0500

    ИИ чууточку умнее

--- gameEngine.py
+++ gameEngine.py
@@ -16,41 +16,60 @@ class GameEngine:
         self.move_count = 0
         self.field = GridLayout(cols=dimension)
         for i in range(dimension * dimension):
-            row_pos = dimension - (i // dimension) - 1
-            column_pos = dimension - (i % dimension) - 1
+            row_pos = (i // dimension)
+            column_pos = i % dimension
+            print('create: ', i, row_pos, column_pos)
             self.field.add_widget(Cell(self, i, row_pos, column_pos))
 
     def get_optimal_cell_after(self, cell):
         my_row = cell.row_pos
         my_column = cell.column_pos
+        print('my: ', my_row, my_column)
         result = None
+        row_result = None
+        column_result = None
         has_enemy_in_line = False
+        me_in_row = 0
+        me_in_column = 0
         potential_position = None
         for i in range(self.dimension):
             current_cell = self.get_cell(i, my_column)
             if current_cell.is_enemy:
                 has_enemy_in_line = True
-            elif not current_cell.is_my:
+            elif current_cell.is_my:
+                me_in_row += 1
+            else:
                 potential_position = i
-        if potential_position is not None and not has_enemy_in_line:
-            result = self.get_cell(potential_position, my_column)
-            print('row potential', result.row_pos)
+        if (potential_position is not None) and (not has_enemy_in_line):
+            row_result = self.get_cell(potential_position, my_column)
+            print('row potential', row_result.row_pos, row_result.column_pos)
+
+        has_enemy_in_line = False
+        potential_col_position = None
+        for i in range(self.dimension):
+            current_cell = self.get_cell(my_row, i)
+            if current_cell.is_enemy:
+                has_enemy_in_line = True
+            elif current_cell.is_my:
+                me_in_column += 1
+            else:
+                potential_col_position = i
+        if potential_col_position is not None and not has_enemy_in_line:
+            column_result = self.get_cell(my_row, potential_col_position)
+            print('column potential', column_result.row_pos, column_result.column_pos)
+
+        if me_in_row >= my_column:
+            result = row_result
         else:
-            for i in range(self.dimension):
-                current_cell = self.get_cell(my_row, i)
-                if current_cell.is_enemy:
-                    has_enemy_in_line = True
-                elif not current_cell.is_my:
-                    potential_position = i
-            if potential_position is not None and not has_enemy_in_line:
-                result = self.get_cell(potential_position, my_column)
-                print('column potential', result.column_pos)
+            result = column_result
         return result
 
     def get_cell(self, x, y):
-        revers_array = self.field.children[::-1]
-        index = x * (self.dimension - 1) + y + x
-        return revers_array[index]
+        result = None
+        for cell in self.field.children:
+            if cell.row_pos == x and cell.column_pos == y:
+                result = cell
+        return result
 
     def stop_game(self):
         self.state = 1
