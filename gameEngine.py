6d18bc511ec27d524eb55a42fec02399d846ab06
nafisgizatov <el_bruho@mail.ru>
2020-06-28 19:47:06 +0500

    ИИ чууточку умнее

--- gameEngine.py
+++ gameEngine.py
@@ -16,32 +16,65 @@ class GameEngine:
         self.move_count = 0
         self.field = GridLayout(cols=dimension)
         for i in range(dimension * dimension):
-            self.field.add_widget(Cell(self, i))
+            row_pos = dimension - (i // dimension) - 1
+            column_pos = dimension - (i % dimension) - 1
+            self.field.add_widget(Cell(self, i, row_pos, column_pos))
+
+    def get_optimal_cell_after(self, cell):
+        my_row = cell.row_pos
+        my_column = cell.column_pos
+        result = None
+        has_enemy_in_line = False
+        potential_position = None
+        for i in range(self.dimension):
+            current_cell = self.get_cell(i, my_column)
+            if current_cell.is_enemy:
+                has_enemy_in_line = True
+            elif not current_cell.is_my:
+                potential_position = i
+        if potential_position is not None and not has_enemy_in_line:
+            result = self.get_cell(potential_position, my_column)
+            print('row potential', result.row_pos)
+        else:
+            for i in range(self.dimension):
+                current_cell = self.get_cell(my_row, i)
+                if current_cell.is_enemy:
+                    has_enemy_in_line = True
+                elif not current_cell.is_my:
+                    potential_position = i
+            if potential_position is not None and not has_enemy_in_line:
+                result = self.get_cell(potential_position, my_column)
+                print('column potential', result.column_pos)
+        return result
 
     def get_cell(self, x, y):
-        revers_array = self.field.children[::-1].copy()
+        revers_array = self.field.children[::-1]
         index = x * (self.dimension - 1) + y + x
         return revers_array[index]
 
     def stop_game(self):
         self.state = 1
     
-    def enemy_move(self):
+    def enemy_move(self, my_cell):
         '''
             Действия противника
         '''
-        for cell in self.field.children:
-            if cell.text == '':
-                cell.text = 'o'
-                cell.is_enemy = True
-                break
+        potential_move = self.get_optimal_cell_after(my_cell)
+        if potential_move is None:
+            for cell in self.field.children:
+                if cell.text == '':
+                    potential_move = cell
+                    break
+
+        potential_move.text = 'o'
+        potential_move.is_enemy = True
         self.move_count += 1
         self.check_state()
 
-    def my_move(self):
+    def my_move(self, cell):
         self.move_count += 1
         if self.check_state():
-            self.enemy_move()
+            self.enemy_move(cell)
 
     def check_state(self):
         '''
